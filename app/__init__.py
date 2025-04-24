3edb134e3f9c674a5363041007693cd0a7b4b701
pusiapaker <antek.chelstowski@gmail.com>
2025-04-24 16:38:09 -0400

    fix: Not Found Error

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from config import config_by_name
 
 def create_app(config_name="dev"):
     app = Flask(__name__)
-    from config import config_by_name
     app.config.from_object(config_by_name[config_name])
 
-    from .routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    from app.routes import home
+    app.register_blueprint(home.bp)
 
     return app
--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-import os
-
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
-    DEBUG = False
-
-class DevelopmentConfig(Config):
     DEBUG = True
+    SECRET_KEY = "dev"
 
 config_by_name = {
-    "dev": DevelopmentConfig
+    "dev": Config
 }
